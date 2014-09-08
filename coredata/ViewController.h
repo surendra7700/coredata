990414cbf1ca3d69e6c10de82bb770e6f2ea6e4c
Jinal <jinal@Jinal.local>
2014-09-08 15:04:49 +0530

    Initial Commit

---
+++ coredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredata
+//
+//  Created by Jinal on 08/09/14.
+//  Copyright (c) 2014 sttl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coredata/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  coredata
+//
+//  Created by Jinal on 08/09/14.
+//  Copyright (c) 2014 sttl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
